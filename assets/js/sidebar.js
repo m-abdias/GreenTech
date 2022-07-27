d50167bac640160934514c83b2fdb0a43e6a24a2
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-26 21:46:08 -0300

    :sparkles: feat: Concluída toda a estrutura, estilização e funcionamento básico do menu lateral do dashboard

---
+++ assets/js/sidebar.js
@@ -0,0 +1,26 @@
+let show = false
+
+const body = document.querySelector('body')
+const sidebar = body.querySelector('aside')
+const toggle = body.querySelector('.toggle')
+const modeSwitch = body.querySelector('.toggle-switch')
+const modeText = body.querySelector('.mode-text')
+const logo = document.querySelector('.header__logo-box')
+
+toggle.addEventListener('click', () => {
+  sidebar.classList.toggle('close', show)
+  show
+    ? (logo.innerHTML = `<span class="image"> LG </span>`)
+    : (logo.innerHTML = `<span class="image"> LOGOTIPO </span>`)
+  show = !show
+})
+
+modeSwitch.addEventListener('click', () => {
+  body.classList.toggle('dark')
+
+  if (body.classList.contains('dark')) {
+    modeText.innerText = 'Dark mode'
+  } else {
+    modeText.innerText = 'Light mode'
+  }
+})
