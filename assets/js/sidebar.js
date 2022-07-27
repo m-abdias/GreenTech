59dbf933bd2206bc560bbf458419859874d855a2
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-27 13:05:54 -0300

    :sparkles: feat: Sistema de ativaão do icone clickado (sidebar) e mudança de seção no layout

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,7 @@
 let show = false
 
 const linkItems = document.querySelectorAll('.sidebar li a')
+const sectionsDashboard = document.querySelectorAll('.section-Dashboard')
 
 const body = document.querySelector('body')
 const sidebar = body.querySelector('aside')
@@ -29,3 +30,22 @@ modeSwitch.addEventListener('click', () => {
   }
 })
 
+// active icon sidebar
+linkItems.forEach((linkItem, index) => {
+  linkItem.addEventListener('click', () => {
+    console.log(index)
+    document.querySelector('.active').classList.remove('active')
+    linkItem.classList.add('active')
+
+    showSectionDashboard(index)
+  })
+})
+
+//evibe a seção correspondente à aba clickada
+function showSectionDashboard(index) {
+  sectionsDashboard.forEach((section, i) => {
+    sectionsDashboard[i].style.display = 'none'
+  })
+
+  sectionsDashboard[index].style.display = 'flex'
+}
