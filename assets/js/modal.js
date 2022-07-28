cf8885513112b30c577968c5c903e4bca0b3bcb8
José Ronaldo <dev.joseronaldo@gmail.com>
2022-07-28 14:48:29 -0300

    :sparkles: feat: Criada toda a estrutura e estilização do modal informativo dos alimentos (aba estoque)

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,13 +1,43 @@
-const openModalButton = document.querySelector('.item__btn')
-const closeModalButton = document.querySelector('#close-modal')
-const modal = document.querySelector('#modal')
-const fade = document.querySelector('#fade')
+// modal da aba produtos
+const openModalButtonProducts = document.querySelector('.item__btn')
+const closeModalButtonProducts = document.querySelector('#close-modal')
+const modalProducts = document.querySelector('#modal')
+const fadeProducts = document.querySelector('#fade')
 
-const toggleModal = () => {
-  modal.classList.toggle('hide')
-  fade.classList.toggle('hide')
+//modal da aba estoque
+const openModalButtonInventory = document.querySelector('.inventory__item')
+const closeModalButtonInventory = document.querySelector(
+  '#close-modal-inventory'
+)
+const modalInventory = document.querySelector('#modalInventory')
+const fadeInventory = document.querySelector('#fadeInventory')
+
+function showAndCloseModal(modal, fade, closeModalButton, openModalButton) {
+  const toggleModal = () => {
+    modal.classList.toggle('hide')
+    fade.classList.toggle('hide')
+  }
+
+  ;[openModalButton, closeModalButton, fade].forEach(el => {
+    el.addEventListener('click', () => toggleModal())
+  })
 }
 
-;[openModalButton, closeModalButton, fade].forEach(el => {
-  el.addEventListener('click', () => toggleModal())
-})
+showAndCloseModal(
+  modalProducts,
+  fadeProducts,
+  closeModalButtonProducts,
+  openModalButtonProducts
+)
+console.log(
+  modalInventory,
+  fadeInventory,
+  closeModalButtonInventory,
+  openModalButtonInventory
+)
+showAndCloseModal(
+  modalInventory,
+  fadeInventory,
+  closeModalButtonInventory,
+  openModalButtonInventory
+)
